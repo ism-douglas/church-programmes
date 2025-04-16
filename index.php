53c100acb6760dd4adc6ac0af89266c5335d6ba7
Douglas Sabwa Indumwa <dglsdeveloper@gmail.com>
2025-04-16 12:15:58 +0300

    Update: Add both name and service to isset before response

--- index.php
+++ index.php
@@ -9,9 +9,8 @@ $phoneNumber = $_POST["phoneNumber"];
 $text        = $_POST["text"];
 
 // Check if the text variable is empty (i.e., initial request).
-// Displays the main menu with options for the user to select from.
 if ($text == "") {
-    // Display main menu
+    // Displays the main menu with options for the user to select from.
     $response  = "CON Welcome to My Church USSD service:\n";
     $response .= "1. Bible Study\n";
     $response .= "2. Counselling\n";
@@ -20,17 +19,16 @@ if ($text == "") {
     $response .= "5. Evangelism\n";
 
 } else {
+    // Splits the text input based on the * character, creating an array ($explodedText).
     $explodedText = explode("*", $text);
     if (count($explodedText) == 1) {
+        // If the array has one element, it prompts the user to enter their name.
         $selectedOption = $explodedText[0];
         $response  = "CON Please enter your name:\n";
     } else if (count($explodedText) == 2) {
-        $selectedOption = $explodedText[0];
-        $name = $explodedText[1];
-        // Splits the text input based on the * character, creating an array ($explodedText).
-        // If the array has one element, it prompts the user to enter their name.
         // If the array has two elements, it proceeds to determine the selected option and the name entered.
-    
+        $selectedOption = $explodedText[0];
+        $name = $explodedText[1];    
 
         // Determine selected service
         if ($selectedOption == "1") {
@@ -44,21 +42,23 @@ if ($text == "") {
         } else if ($selectedOption == "5") {
             $service = "Evangelism";
         } else {
+            // If the service selection is invalid, it ends the session with an "Invalid selection" message.
             $response = "END Invalid selection.";
         }
-        // Determines which service the user selected based on the first element of the array.
 
-        if (isset($service)) {
-            $response = "END Thank you $name, you have registered for $service.";
-        // Constructs the final response, thanking the user and confirming their selection.
-        // If the service selection is invalid, it ends the session with an "Invalid selection" message.
+        //If name and service are set
+        if (isset($name,$service)) {
+            // Constructs the final response, thanking the user and confirming their selection.
+            $response = "END Thank you $name, you have registered for $service.";       
+        
         }
     }
 }
 
-// Echo the response
+// Sets the response header again (to ensure proper content type) and outputs the response.
 header('Content-type: text/plain');
+// Echo the response
 echo $response;
 
-// Sets the response header again (to ensure proper content type) and outputs the response.
+
 ?>
